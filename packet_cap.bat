c8c0ac2c0ffda289c330033217ee9bca052ea469
23topi <94148487+23topi@users.noreply.github.com>
2022-05-07 16:08:49 +0200

    easy packet tracer, network capture with netsh
    easy packet tracer with netsh as batchfile, just type in the network path where netsh should store the capture-file. Just psh enter when you want to stop the capturing.

---
+++ packet_cap.bat
@@ -0,0 +1,11 @@
+@echo "Bitte einen Namen und Pfad fuer die tracedatei eingeben z.b. c:\trace.etl"
+
+@echo off
+Set /p tracedatei=
+
+@ECHO OFF
+netsh trace start capture=yes tracefile=%tracedatei% persistent=yes maxsize=4096
+
+@ECHO Wenn Sie das Netzwerkmonitoring beenden moechten bitte Enter druecken
+PAUSE
+netsh trace stop
