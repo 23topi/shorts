6057713fd9dba127fac99f26ecd731c911d6baa6
23topi <94148487+23topi@users.noreply.github.com>
2022-06-07 21:28:11 +0200

    Update and rename CVE202230190.ps1 to Follina.ps1

--- Follina.ps1
+++ Follina.ps1
@@ -1,4 +1,4 @@
-# Delete Ms diagnostic tool registry key (vulnerability CVE2022-30190)
+# Delete MS support diagnostic tool registry key (vulnerability CVE2022-30190)
 
 # New-PSDrive -Name HKCR -PSProvider Registry -Root HKEY_CLASSES_ROOT
 $testdatei =  "C:\*.reg"       # "C:\ms-msdt_ps_reg_ex.reg_$(get-date -f yyyyMMdd).reg"
