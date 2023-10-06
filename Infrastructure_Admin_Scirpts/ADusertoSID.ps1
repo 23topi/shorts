ad3ee7f56af62a866b2e880633320a413a6f4cb5
23topi <94148487+23topi@users.noreply.github.com>
2023-10-06 08:18:12 +0200

    Delete Infrastructure_Admin_Scirpts directory

--- Infrastructure_Admin_Scirpts/ADusertoSID.ps1
+++
@@ -1,6 +0,0 @@
-﻿$DOMAIN = "Domain"
-$USERNAME = "Name"
-
-$objUser = New-Object System.Security.Principal.NTAccount($DOMAIN, $USERNAME)
-$strSID = $objUser.Translate([System.Security.Principal.SecurityIdentifier])
-$strSID.Value
--- Infrastructure_Admin_Scirpts/SIDtoADuser.ps1
+++
@@ -1,9 +0,0 @@
-﻿$SID = "SID"
-
-$objSID = New-Object System.Security.Principal.SecurityIdentifier($SID)
-$objUser = $objSID.Translate([System.Security.Principal.NTAccount])
-$objUser.Value
-
-
-
-# S-1-12-1-...
