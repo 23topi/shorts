57365e487493c81895fd5a1539eca04364ce2f6d
23topi <94148487+23topi@users.noreply.github.com>
2023-10-06 08:11:14 +0200

    Add files via upload

---
+++ Infrastructure_Admin_Scirpts/SIDtoADuser.ps1
@@ -0,0 +1,9 @@
+﻿$SID = "S-1-12-1-2952765392-1128664258-1674567319-1539630735"
+
+$objSID = New-Object System.Security.Principal.SecurityIdentifier($SID)
+$objUser = $objSID.Translate([System.Security.Principal.NTAccount])
+$objUser.Value
+
+
+
+# S-1-12-1-2952765392-1128664258-1674567319-1539630735
---
+++ Infrastructure_Admin_Scirpts/ps_wlan_SSID_signal-strength.ps1
@@ -0,0 +1,24 @@
+﻿while ($true) {
+    $networks = netsh wlan show interfaces
+    $ssid = $null
+    $signalStrength = $null
+
+    foreach ($line in $networks) {
+
+    if ($line -match "SSID") {
+        $ssid = $line -replace "SSID\s+:\s+", ""
+        Write-Host "SSID: $ssid"
+        }
+        if ($line -match "Signal") {
+            $signalStrength = $line -replace "Signal\s+:\s+", ""
+        }
+    }
+
+    if ($ssid -and $signalStrength) {
+        Write-Host "SSID: $ssid, Signalstärke: $signalStrength"
+    } else {
+        Write-Host "Kein WLAN-Netzwerk gefunden."
+    }
+
+    Start-Sleep -Seconds 5  # Aktualisierung alle 5 Sekunden (kann anpassen werden)
+}
